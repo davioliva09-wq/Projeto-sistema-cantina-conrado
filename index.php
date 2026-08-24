771158d01987a8bb30f8e67bf135d4f0f21185a3
Axilition <eduaugustogames@gmail.com>
2026-08-24 10:52:05 -0300

    Index Semi-concluído

--- index.php
+++ index.php
@@ -22,34 +22,52 @@
 
 <?php include 'header.php'; ?>
 
-    <main>
+    <main style="position: relative; overflow: hidden;">
 
-        <section class="section-login">
-           <!-- <p style="color: #ffa805;">BEM VINDO(A)!</p>-->
+        <section class="section-index">
 
-            <h1 class="intro">
+            <p style="color: #ffa805; font-family: Fredoka; font-weight: 600; font-size: 20px;">
+            BEM VINDO(A)!
+            </p>
+
+
+
+            <h1 class="intro" style="font-size: 70px;">
                 Sua fome não espera,
                 <br>
                 <span class="span-intro">a gente facilita!</span>
             </h1>
 
             <p>
-                Com o Sistema de Cantina Conrado, você consulta o
-                cardápio, faz seus pedidos e acompanha suas compras
+                Com o Sistema de Cantina Conrado, você consulta o <br>
+                cardápio, faz seus pedidos e acompanha suas compras <br>
                 de forma rápida, prática e sem filas.
             </p>
 
             <button class="btn-intro">
-                <a href="login.php?tipo=usuario" >
-                  FAZER LOGIN
+                <a href="cadastro.php" style="color:navy">
+                  CADASTRE-SE JÁ!
                  </a>
             </button>
 
-            <p class="text-intro">
-                Acesso exclusivo para alunos, responsáveis,
-                funcionários e administradores.
-            </p>
+            <div style="display: flex; align-items: center; margin-top: 15px;">
+                <img src="images/miku-lock.png" alt="cadeado UwU" style="width: 15px; height: auto; margin-right: 5px;">
+                <p class="text-intro">
+                Cadastro exclusivo para alunos, responsáveis,
+                e funcionários.
+                </p>
+            </div>
+
         </section>
+
+        <section class="lanches-index">
+
+        <img src="https://i.imgur.com/SPapSq8.png" alt="lanches gulosos">
+
+        </section>
+
+        <div class="circle-superior-direito"></div>
+        <div class="circle-inferior-esquerdo"></div>
 
 
     </main>
