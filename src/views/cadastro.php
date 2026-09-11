4eb5742ffc54def968c49b00930abd3057b768cc
Davi Oliva <seuemail@gmail.com>
2026-09-10 22:11:04 -0300

    ïmplementada a busca de usuarios

--- src/views/cadastro.php
+++ src/views/cadastro.php
@@ -3,6 +3,17 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     require_once("processar-cadastro.php");
     header("location:login.php");
     exit();
+
+    $email = $_POST["email"];
+
+if ($usuario->emailExiste($email)) {
+    echo "Erro: Este e-mail já está cadastrado!";
+    exit;
+} else {
+    $usuario->cadastrar('Davi Oliva', $email, '', 'Conrado@123');
+    echo "Cadastro realizado com sucesso!";
+}
+
 }
 ?>
 <!DOCTYPE html>
